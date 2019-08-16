5fd91dfc91bbc580404e72db45b88a563cc3dac0
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-08-16 17:14:05 -0300

    Tentativa de algoritmo Groger Bank

---
+++ groger_bank.py
@@ -0,0 +1,26 @@
+print('Bem vindo(a) ao GROGER BANK, para começar digite suas informações: ')
+nome = input('Digite seu nome completo: ')
+idade = input('Digite sua idade: ')
+saldo = float(input('(Simulação de Saldo) - Digite a simulação de saldo: '))
+
+
+def saque(saldo):
+  print("Digite (1) para saque")
+  valor = float(input('Digite o valor do saque: ')
+
+     # if valor <= 1000:
+    #  print('Saque permitido')
+      #saldo = saldo - valor
+  )
+
+  print("Digite (2) para deposito")
+
+  print("Digite (3) para emprestimo")
+
+  print("Digite (4) para vizualizar informações")
+
+  print("Digite (qualquer outro caracter) para sair")
+
+
+
+
